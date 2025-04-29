e25b2b73c088b9e1845989d286b0058719d23ccb
ADEL\adool <adoolfaki@gmail.com>
2025-04-29 16:03:20 +0300

    refactory final one

--- H2BR0/H2BR0.h
+++ H2BR0/H2BR0.h
@@ -31,7 +31,7 @@
 #define	MODULE_PN		_H2BR0
 
 /* Port-related Definitions */
-#define	NUM_OF_PORTS	6
+#define	NUM_OF_PORTS	5
 #define P_PROG 			P2		/* ST factory bootloader UART */
 
 /* Define Available Ports */
