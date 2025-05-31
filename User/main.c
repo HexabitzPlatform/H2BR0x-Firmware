4c8672ebbf8e938202dcf5ed18ba4658345fd742
Mahmood_Reda <mr4141705@gmail.com>
2025-05-31 16:05:46 +0300

    clear topology & main files .

--- User/main.c
+++ User/main.c
@@ -1,38 +1,38 @@
 /*
- BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2024 Hexabitz
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
  File Name     : main.c
  Description   : Main program body.
  */
-/* Includes ------------------------------------------------------------------*/
+
+/* Includes ****************************************************************/
 #include "BOS.h"
 
-/* Private variables ---------------------------------------------------------*/
+/* Private variables *******************************************************/
 
-/* Private function prototypes -----------------------------------------------*/
+/* Private Function Prototypes *********************************************/
 
-/* Main function ------------------------------------------------------------*/
-
+/* Main Function ***********************************************************/
 int main(void){
 
-	Module_Init();		//Initialize Module &  BitzOS
+	/* Initialize Module &  BitzOS */
+	Module_Init();
 
-	//Don't place your code here.
-	for(;;){}
-}
-
-/*-----------------------------------------------------------*/
-
-/* User Task */
-void UserTask(void *argument){
-	EXG_Init(EMG);
-	
-	// put your code here, to run repeatedly.
-	while(1){
-//		PlotToTerminal(P3);
-//		HAL_Delay(5000);
+	/* Don't place your code here */
+	for(;;){
 	}
 }
 
-/*-----------------------------------------------------------*/
+/***************************************************************************/
+/* User Task */
+void UserTask(void *argument){
+
+	/* put your code here, to run repeatedly. */
+	while(1){
+
+	}
+}
+
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
